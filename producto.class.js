e1e82f6ff0507dc857458fba0b9d9a4a118c900d
doblefilmina <brusafederico@gmail.com>
2020-05-07 21:33:36 -0300

    Obteniendo datos via AJAX y leyendolos con fetch

---
+++ producto.class.js
@@ -0,0 +1,53 @@
+// 1) los objetos se definen y se organizan en una estructura que se llama "class"	
+class Producto {
+    // 2) "constructor" están en el interior de las clases y se configuran: qué propiedades configuran al objeto y qué valor toma esa propiedad.
+                constructor(n, s, p, i, m) {
+                    this.nombre = n
+                    this.stock = s
+                    this.precio = p
+                    this.imagen = i
+                    this.marca = m
+            }
+
+            AgregarImagen() {
+                 console.log(this)
+                    this.foto = this.nombre.replace(/ /g, "-")
+                    this.foto = this.foto.toLowerCase()
+            }	
+
+            AgregarFuente () {
+                this.fuente = this.nombre.replace(/ /g, "-")
+                this.fuente = this.fuente.toLowerCase()
+                this.fuente = `https://source.unsplash.com/200x200/?${this.fuente}`
+            }
+
+            Mostrar() {
+                console.log("El que ejecuto este metodo es:")
+                console.log(this)	 
+                
+                let ficha = document.createElement("article")
+                    ficha.classList.add("col-4")
+
+                    ficha.innerHTML = `
+                            <div class="card h-100">
+                                <a href="#">
+                                    <img class="card-img-top img-fluid" src="${this.imagen}"  alt="${this.nombre}">
+                                </a>
+                                <div class="card-body">
+                                    <h4 class="card-title"><a href="#">${this.nombre}</a> <span class="badge badge-pill badge-success float-right">$${this.precio.toFixed(2)}</span></h4>
+                                    <p class="card-text">${this.stock} unid.</p>
+                                    <button class="btn btn-primary float-right">Comprar</button>
+                                </div>
+                            </div> `
+
+                    document.querySelector("#productos-destacados").appendChild(ficha)		
+                    
+                }	
+
+                Descuento(cupon) {
+                    if (cupon == "AUH2020") {
+                        this.precio = this.precio * (1-0.15)
+                    }
+
+                }		
+            }
